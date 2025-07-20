c34b41963b04b242c504f992b5500c5ae8aa2b7d
neil-stead <neilstead@zoho.com>
2025-07-20 20:41:14 +0400

    feat: connected db to index and displaying content

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import sqlite3
 
 # Configure app
 app = Flask(__name__)
 
+
+DATABASE = "db/vocab.db"
+
+def get_db_connection():
+    if 'db' not in g:
+        g.db = sqlite3.connect(DATABASE)
+    return g.db
+
+@app.teardown_appcontext
+def close_db_connection(exception):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
+
+
 @app.route("/")
-def index():
-    return render_template("index.html")
+def show_words():    
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM vocabulary LIMIT 5")
+    rows = cursor.fetchall()
+    return render_template("index.html", rows=rows)
+
